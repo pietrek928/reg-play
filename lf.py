41d7366b69f0ec8cdcd400c28d82544144242809
Pietrek xD <pietrek.j@gmail.com>
2024-11-24 16:18:01 +0100

    fix lf model

--- lf.py
+++ lf.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from gc import collect
 from typing import Dict, Tuple
 import torch
 
@@ -18,14 +19,14 @@ class LFModelSimple:
         v1 = v + w*p[:, 0]
         v2 = v - w*p[:, 0]
 
-        am1 = p[:, 1]*u1 + p[:, 2]*v1
+        am1 = p[:, 3]*(u1 - p[:, 1]*v1)
         am1 = torch.where(am1 > 0, (am1 - p[:, 4]).clamp(min=0), (am1 + p[:, 4]).clamp(max=0))
 
-        am2 = p[:, 1]*u2 + p[:, 2]*v2
+        am2 = p[:, 3]*(u2 - p[:, 2]*v2)
         am2 = torch.where(am2 > 0, (am2 - p[:, 4]).clamp(min=0), (am2 + p[:, 4]).clamp(max=0))
 
         dv = am1 + am2
-        dw = p[:, 3] * (am1 - am2)
+        dw = p[:, 5] * (am1 - am2)
         return {
             'v': dv,
             'w': dw,
@@ -56,19 +57,27 @@ def score_sim_fit(
         step_func, score_func, state: Dict[str, torch.Tensor],
         samples: Dict[str, torch.Tensor], out_keys: Tuple[str, ...]
 ):
+    state = {
+        k: v.clone() for k, v in state.items()
+    }
     scores = []
     n = tuple(samples.values())[0].shape[-1]
     for it in range(n):
+        if it % 100 == 0:
+            collect()
+
         in_data = {
             k: v[:, it]
             for k, v in samples.items()
             if k not in out_keys
         }
         out_dx = step_func(in_data | state)
-        state = {
-            k: v + out_dx[k] * in_data['dt']
-            for k, v in state.items()
-        }
+        # state = {
+        #     k: v + out_dx[k] * in_data['dt']
+        #     for k, v in state.items()
+        # }
+        for k in state.keys():
+            state[k] += out_dx[k] * in_data['dt']
         out_gt = {
             k: v[:, it]
             for k, v in samples.items()
@@ -107,18 +116,24 @@ def run_model_sim(
     }
 
 
-def prepare_lf_data(fname):
+def prepare_lf_data(fname, device='cpu'):
     import pandas as pd
 
     df = pd.read_excel(fname)
-    dt = torch.from_numpy(df['dt'].to_numpy()).to(torch.float64).unsqueeze(0)
+    dt = torch.from_numpy(df['dt'].to_numpy()).to(
+        dtype=torch.float64, device=device
+    ).unsqueeze(0)
     t = torch.cumsum(dt, -1)
 
-    angle_l = torch.from_numpy(df['angle_l'].to_numpy()).to(torch.float64).unsqueeze(0)
+    angle_l = torch.from_numpy(df['angle_l'].to_numpy()).to(
+        dtype=torch.float64, device=device
+    ).unsqueeze(0)
     drot_l = compute_drot(angle_l)
     rot_l = accum_drot(drot_l)
 
-    angle_r = torch.from_numpy(df['angle_r'].to_numpy()).to(torch.float64).unsqueeze(0)
+    angle_r = torch.from_numpy(df['angle_r'].to_numpy()).to(
+        dtype=torch.float64, device=device
+    ).unsqueeze(0)
     drot_r = -compute_drot(angle_r)
     rot_r = accum_drot(drot_r)
 
@@ -128,11 +143,15 @@ def prepare_lf_data(fname):
         # drot_l=drot_l,
         rot_l=rot_l,
         dl=rot_l * (.02 * torch.pi),
-        u_l=torch.from_numpy(df['motor_l_u'].to_numpy()).to(torch.float64).unsqueeze(0),
+        u_l=torch.from_numpy(df['motor_l_u'].to_numpy()).to(
+            dtype=torch.float64, device=device
+        ).unsqueeze(0),
         # drot_r=drot_r,
         rot_r=rot_r,
         dr=rot_r * (.02 * torch.pi),
-        u_r=torch.from_numpy(df['motor_r_u'].to_numpy()).to(torch.float64).unsqueeze(0),
+        u_r=torch.from_numpy(df['motor_r_u'].to_numpy()).to(
+            dtype=torch.float64, device=device
+        ).unsqueeze(0),
     )
 
 
@@ -192,6 +211,10 @@ def score_lf_fit(state_gt, state_sim):
     )
 
 
+def validate_lf_params(params):
+    return torch.all(params > 0, dim=-1)
+
+
 def score_points(params):
     model = LFModelSimple(params)
     z = torch.zeros_like(params[..., 0])
@@ -204,38 +227,69 @@ def score_points(params):
 
 # TODO: better interpolation
 def add_randoms(pts, a):
-    v = pts.std(dim=0)
+    v = pts.std(dim=0) + pts.abs().mean(dim=0) * .25
     r = torch.rand_like(pts) - 0.5
     return pts + r * (v * a)
 
 
+def update_momentum(m, a, pts_old, scores_old, pts_new, scores_new):
+    score_diff = scores_new - scores_old
+    pts_diff = pts_new - pts_old
+    return m + pts_diff * score_diff.unsqueeze(-1) * a
+
+
 # TODO: better selection
 # first value is score
-def select_min(pts, n):
+def select_min_indexes(scores, n):
     # remove invalid values first
-    finite_mask = torch.isfinite(pts)
-    finite_row_mask = torch.all(finite_mask, dim=1)
-    pts = pts[finite_row_mask]
+    indexes = torch.arange(scores.shape[0], dtype=torch.int32, device=scores.device)
+    finite_mask = torch.isfinite(scores)
+    scores = scores[finite_mask]
+    indexes = indexes[finite_mask]
 
-    pts_order = pts[..., 0].sort(dim=0).indices
-    pts = pts[pts_order]
-    selected = pts[:n//2]
-    rest = pts[n//2:]
-    return torch.cat([selected, rest[torch.randperm(rest.shape[0])[:n//2]]], dim=0)
+    pts_order = scores.sort().indices
+    indexes = indexes[pts_order]
+    selected = indexes[:n//2]
+    rest = indexes[n//2:]
+    return torch.cat([selected, rest[torch.randperm(rest.shape[0], device=scores.device)[:n//2]]], dim=0)
 
 
-def search_step(scored_points, n, a):
+def search_step(old_pts, old_scores, old_moments, n, a, b):
     new_pts = torch.cat([
-        add_randoms(scored_points[..., 1:], a),
-        add_randoms(scored_points[..., 1:], a*.3),
-        add_randoms(scored_points[..., 1:], a*.1),
-        add_randoms(scored_points[..., 1:], a*.03),
-        add_randoms(scored_points[..., 1:], a*.01),
+        add_randoms(old_pts, a),
+        old_pts + old_moments * b,
+        add_randoms(old_pts, a*.3),
+        old_pts + old_moments * b * .3,
+        add_randoms(old_pts, a*.1),
+        old_pts + old_moments * b * .1,
+        add_randoms(old_pts, a*.03),
+        old_pts + old_moments * b * .03,
+        add_randoms(old_pts, a*.01),
+        old_pts + old_moments * b * .01,
+        add_randoms(old_pts, a*.003),
+        old_pts + old_moments * b * .003,
+        add_randoms(old_pts, a*.001),
+        old_pts + old_moments * b * .001,
     ], dim=0)
-    scores = score_points(new_pts)
-    scored_new_pts = torch.cat([scores.unsqueeze(dim=-1), new_pts], dim=-1)
-    all_scored_pts = torch.cat([scored_points, scored_new_pts], dim=0)
-    return select_min(all_scored_pts, n)
+    prev_pts = torch.cat([old_pts] * 14, dim=0)
+    prev_scores = torch.cat([old_scores] * 14, dim=0)
+    prev_moments = torch.cat([old_moments] * 14, dim=0)
+
+    valid_indexes = validate_lf_params(new_pts)
+    new_pts = new_pts[valid_indexes]
+    prev_pts = prev_pts[valid_indexes]
+    prev_scores = prev_scores[valid_indexes]
+    prev_moments = prev_moments[valid_indexes]
+
+    new_scores = score_points(new_pts)
+    new_moments = update_momentum(prev_moments, .2, prev_pts, prev_scores, new_pts, new_scores)
+
+    all_pts = torch.cat([old_pts, new_pts], dim=0)
+    all_scores = torch.cat([old_scores, new_scores], dim=0)
+    all_moments = torch.cat([old_moments, new_moments], dim=0)
+
+    selected_indexes = select_min_indexes(all_scores, n)
+    return all_pts[selected_indexes], all_scores[selected_indexes], all_moments[selected_indexes]
 
 
 def vis_params(data, params):
@@ -254,20 +308,28 @@ def vis_params(data, params):
     # )
     # print(sim_score)
 
-
 data = prepare_lf_data('/home/pietrek/Downloads/mcal.xlsx')
-vis_params(data, [-0.006669893307789543, 108.69518660320526, -5.014715774113545, 1.0924990184551613, 3.985143622559254])
+vis_params(data, [2.165925225621571, 0.0516630785650092, 0.041066706577213015, 112.50283206725963, 0.7414677216321874, 0.506700264484151])
+
+
+# device='cuda:0'
+# # device='cpu'
+# data = prepare_lf_data('/home/pietrek/Downloads/mcal.xlsx', device=device)
 
 # params = torch.tensor([
-#     [1e9, 1, 90, -4, 20, 3],
-#     [1e9, 2, 80, -5, 15, 4],
-# ], dtype=torch.float64)
+#     [0.9386564921990348, 0.0510397941187321, 0.04097628708440863, 149.38405005788226, 9.72265956724609, 0.2052234886348298],
+#     # [.5, -.1, 100, 50, 80, .1, 1],
+#     [0, 0, 0, 0, 0, 0]
+# ], dtype=torch.float64, device=device) + .01
+# scores = torch.tensor([1e9, 1e9], dtype=torch.float64, device=device)
+# moments = torch.zeros_like(params)
 
-# a = 4
+# a = 1
+# b = 5
 # for it in range(100):
-#     params = search_step(params, 5000, a)
-#     a *= .97
+#     params, scores, moments = search_step(params, scores, moments, 5000, a, b)
+#     # a *= .97
 #     # print(params)
-#     print(it, a, float(params[0, 0]), tuple(map(float, params[0, 1:])))
+#     print(it, a, float(scores[0]), tuple(map(float, params[0])))
 # print(params.shape)
-# print(tuple(map(float, params[0][1:])))
+# print(tuple(map(float, params[0])))
